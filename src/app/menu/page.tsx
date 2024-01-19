8b72a1d3546fa1d234cd77c3ae66e8517eadf610
SnowIguana <Sbaskota71@gmail.coma>
2024-01-19 07:16:57 +0545

    first commit

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const MenuPage = () => {
+  return (
+    <div>
+        <Image src="/open.png" alt= "" width={20} height={20} />
+    </div>
+  )
+}
+
+export default MenuPage
